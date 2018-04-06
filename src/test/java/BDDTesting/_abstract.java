9cfd6028cc0c7c38f6405cada31508d0a03aa348
rjjtan@gmail.com <rjjtan@gmail.com>
2018-04-06 12:47:16 +0800

    add safari browser

--- src/test/java/BDDTesting/_abstract.java
+++ src/test/java/BDDTesting/_abstract.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 import java.util.concurrent.TimeUnit;
 import java.util.Collections;
@@ -12,11 +14,12 @@ public class _abstract {
 
     protected static WebDriver driver;
 
-//CHROME BROWSER
-    protected WebDriver getDriver(){
+// <!------ CHROME BROWSER ------>
+/**    protected WebDriver getDriver(){
         if (driver == null){
             System.setProperty("webdriver.chrome.driver", "/Applications/Google Chrome.app/Contents/MacOS/chromedriver");
             ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-fullscreen");
             options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
             driver = new ChromeDriver(options);
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -24,20 +27,32 @@ public class _abstract {
         }
          return driver;
     }
+**/
 
-
-//FIREFOX BROWSER
-/**    protected WebDriver getDriver(){
+// <!------ FIREFOX ------>
+    protected WebDriver getDriver(){
         if (driver == null){
             System.setProperty("webdriver.gecko.driver", "/Applications/Firefox.app/Contents/MacOS/geckodriver");
             FirefoxOptions firefoxOptions = new FirefoxOptions();
             this.driver = new FirefoxDriver(firefoxOptions);
+            driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             driver.get("http://admin.kinvo.ph/login");
         }
          return driver;
     }
-**/
+
+
+// <!------ SAFARI ------>
+/**      protected WebDriver getDriver(){
+      if (driver == null){
+            WebDriver driver = new SafariDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+            driver.get("http://admin.kinvo.ph/login");
+      }
+         return driver;
+    } **/
 }
 
 
